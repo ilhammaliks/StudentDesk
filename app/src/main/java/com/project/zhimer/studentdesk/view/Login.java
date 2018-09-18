7dd9e3fffbfdf3165e0e9f03e0b1603ae3072640
Rizky Syahferi <syahferiaswan@gmail.com>
2018-09-18 16:32:16 +0700

    add animation

--- app/src/main/java/com/project/zhimer/studentdesk/view/Login.java
+++ app/src/main/java/com/project/zhimer/studentdesk/view/Login.java
@@ -1,11 +1,14 @@
 package com.project.zhimer.studentdesk.view;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +22,11 @@ public class Login extends AppCompatActivity {
 
     String nim, password;
 
+    EditText etNim, etPassword;
+    Button toastFail, blogin;
+
+    Animation fadein, fadeout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +36,18 @@ public class Login extends AppCompatActivity {
 
         final SessionManager sessionManager = new SessionManager(this);
 
-        final EditText etNim = findViewById(R.id.etNim);
-        final EditText etPassword = findViewById(R.id.etPassword);
-        final Button blogin = findViewById(R.id.bLogin);
+        etNim = findViewById(R.id.etNim);
+        etPassword = findViewById(R.id.etPassword);
+        blogin = findViewById(R.id.bLogin);
+
+        //animation toast
+        toastFail = findViewById(R.id.toast_fail);
+        toastFail.setAlpha(0);
+
+        //set animation
+        //animation
+        fadein = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        fadeout = AnimationUtils.loadAnimation(this, R.anim.fadeout);
 
         blogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,8 +82,41 @@ public class Login extends AppCompatActivity {
                     Login.this.startActivity(login);
 
                     finish();
+                } else {
+                    toastFail.setAlpha(1);
+                    toastFail.startAnimation(fadein);
+                    new BackgroundTask().execute();
                 }
             }
         });
+    }
+
+    //asynchronus condition
+    private class BackgroundTask extends AsyncTask {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            toastFail.startAnimation(fadeout);
+            toastFail.setAlpha(0);
+            etNim.setError(null);
+            etPassword.setError(null);
+        }
     }
 }
