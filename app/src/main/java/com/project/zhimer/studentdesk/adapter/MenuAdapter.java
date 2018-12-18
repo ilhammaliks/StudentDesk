69d56585027452b59c31c00a7ba726c988abb10f
zhemus <ilhammalikmuhammad@gmail.com>
2018-12-18 14:06:33 +0700

    draft main menu(continued)

---
+++ app/src/main/java/com/project/zhimer/studentdesk/adapter/MenuAdapter.java
@@ -0,0 +1,67 @@
+package com.project.zhimer.studentdesk.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.project.zhimer.studentdesk.R;
+import com.project.zhimer.studentdesk.model.Menu;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Menu> listMenu;
+    private LayoutInflater inflater = null;
+
+    public MenuAdapter(Context context, List<Menu> listMenu) {
+        this.context = context;
+        this.listMenu = listMenu;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+
+
+    @NonNull
+    @Override
+    public MenuAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_menu, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MenuAdapter.ViewHolder holder, int position) {
+
+        final Menu menu = listMenu.get(position);
+
+        holder.title.setText(menu.getTitle());
+        Picasso.with(context).load(menu.getImage());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listMenu.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title;
+        ImageView icon;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.menu_title);
+            icon = itemView.findViewById(R.id.menu_icon);
+        }
+    }
+}
---
+++ app/src/main/java/com/project/zhimer/studentdesk/model/Menu.java
@@ -0,0 +1,26 @@
+package com.project.zhimer.studentdesk.model;
+
+public class Menu {
+
+    private String title;
+    private int image;
+
+    public Menu() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
